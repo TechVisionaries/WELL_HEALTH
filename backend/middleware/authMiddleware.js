94e05f2ed8718a16aa830bbd0e0b02eb98d2fc36
IT21802126-SilvaGMSS <91774321+IT21802126-SilvaGMSS@users.noreply.github.com>
2024-10-07 13:09:31 +0530

    Update authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 import asyncHandler from "express-async-handler";
 import User from "../models/userModel.js";
 
-const authMiddleware = asyncHandler(async (req, res, next) => {
+const protect = asyncHandler(async (req, res, next) => {
   let token;
   if (
     req.headers.authorization &&
@@ -74,4 +74,4 @@ const isAdmin = asyncHandler(async (req, res, next) => {
 });
 
 //expotation
-export { authMiddleware, isAdmin, isManager, isDoctor, isPatient};
+export { protect, isAdmin, isManager, isDoctor, isPatient};
