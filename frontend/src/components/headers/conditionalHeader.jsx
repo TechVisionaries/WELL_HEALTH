fe0edf6e51fd0088d55a8185b67d7cb8e2439d40
Osadha Ganewaththa <it21802058@my.sliit.lk>
2024-10-19 04:17:48 +0530

    conditional Header updated

--- frontend/src/components/headers/conditionalHeader.jsx
+++ frontend/src/components/headers/conditionalHeader.jsx
@@ -2,6 +2,7 @@ import { useSelector } from "react-redux";
 import Header from "./patientHeader";
 import ManagerHeader from "./managerHeader";
 import DoctorHeader from "./doctorHeader";
+import AdminHeader from "./adminHeader";
 
 const ConditionalHeader = () => {
   const { userInfo } = useSelector((state) => state.auth);
@@ -15,6 +16,9 @@ const ConditionalHeader = () => {
   }else if (userInfo.userType === "doctor") {
     // Render manager-specific header
     return <DoctorHeader />;
+  }else if (userInfo.userType === "admin") {
+    // Render manager-specific header
+    return <AdminHeader />;
   }
 
   // You can add other cases for different user types if necessary
